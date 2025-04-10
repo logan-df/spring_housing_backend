40aa7c852a9748a5460016eed9d1d9416085cc2f
logan-df <ldford711@gmail.com>
2025-04-10 09:11:33 -0400

    changes

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const Joi = require("joi");
 const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "./public/images/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
 
 app.get("/",(req, res)=>{
     res.sendFile(__dirname+"/index.html");
@@ -11,6 +24,7 @@ app.get("/",(req, res)=>{
 
 let houses = [
     {
+    "_id":1,
     "name": "Farmhouse",
     "size": 2000,
     "bedrooms": 3,
@@ -32,6 +46,7 @@ let houses = [
     ]
     },
     {
+    "_id":2,
     "name": "Mountain House",
     "size": 1700,
     "bedrooms": 3,
@@ -57,6 +72,7 @@ let houses = [
     ]
     },
     {
+    "_id":3,
     "name": "Lake House",
     "size": 3000,
     "bedrooms": 4,
@@ -83,6 +99,45 @@ let houses = [
 app.get("/api/houses", (req, res)=>{
     res.send(houses);
 });
+
+app.post("/api/houses", upload.single("img"), (req,res)=>{
+    const result = validateHouse(req.body);
+
+
+    if(result.error){
+        console.log("I have an error");
+        res.status(400).send(result.error.deatils[0].message);
+        return;
+    }
+
+    const house = {
+        _id: houses.length,
+        name:req.body.name,
+        size:req.body.size,
+        bedrooms:req.body.bedrooms,
+        bathrooms:req.body.bathrooms,
+    };
+
+    if(req.file){
+        house.main_image = req.file.filename;
+    }
+
+    houses.push(house);
+    res.status(200).send(house);
+});
+
+const validateHouse = (house) => {
+    const schema = Joi.object({
+        _id:Joi.allow(""),
+        name:Joi.string().min(3).required(),
+        size:Joi.number().required().min(0),
+        bedrooms:Joi.number().required().min(0),
+        bathrooms:Joi.number().required().min(0),
+
+    });
+
+    return schema.validate(house);
+};
 
 app.listen(3001, ()=>{
     console.log("I'm listening");
